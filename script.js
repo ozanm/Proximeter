025335dbefffa5763dc1feb646514bf4c095aaea
ozanmirza1 <ozan.mirza@icloud.com>
2018-06-16 13:26:40 -0400

    0101010101010101011001010101010101001010100101010100101011010010110101001010101010100101010101010101001101010101010100101010101010

--- script.js
+++ script.js
@@ -1,44 +1,70 @@
-// Google GeoLocation API Key: AIzaSyA8pukmW_of-7QT_Y1FH9MkqZOq4X8Ux7o
+window.onload = function() {
+  var Client = require(["predicthq"]);
 
-var surveyTitles = ["First Off... Location!"];
-var address = "";
+  var surveyTitles = ["First Off... Location!"];
+  var address = "";
+  var prefrences = {
+    tech: undefined,
+    art: undefined,
+    history: undefined,
+    math: undefined,
+    sports: undefined,
+    performing_arts: undefined,
+    community: undefined,
+  };
+  var finalDetails = undefined;
 
-document.getElementById('get_started').onclick = function() {
-  document.getElementById('title').style.animation = "exiting 1.5s forwards";
-  document.getElementById('explanation').style.animationDelay = "2.5s";
-  document.getElementById('explanation').style.animation = "exiting 1.5s forwards";
-  document.getElementById('get_started').style.animationDelay = "3.5s";
-  document.getElementById('get_started').style.animation = "exiting 1.5s forwards";
-  document.getElementById('surveyTitle').innerHTML = surveyTitles[0];
-  document.getElementById('surveyTitle').style.animationDelay = "3.5s";
-  document.getElementById('surveyTitle').style.animation = "startSurveyTitle 2.5s forwards";
-  document.getElementById('loading').style.animationDelay = "3.5s";
-  document.getElementById('loading').style.animation = "show 1s forwards";
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      getReverseGeocodingData(position.coords.latitude, position.coords.longitude);
-      setTimeout(function() {
-        document.getElementById('loading').style.opacity = "0";
-      }, 2000);
-    });
+  var coords = {
+    lat: undefined,
+    long: undefined,
   }
-}
 
-function getReverseGeocodingData(lat, lng) {
-    var latlng = new google.maps.LatLng(lat, lng);
-    // This is making the Geocode request
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-        if (status !== google.maps.GeocoderStatus.OK) {
-            alert(status);
-        }
-        // This is checking to see if the Geoeode Status is OK before proceeding
-        if (status == google.maps.GeocoderStatus.OK) {
-            setTimeout(function() {
-              address = results[0].formatted_address;
-              document.getElementById('location').classList.add("locationClass");
-              document.getElementById('location').innerHTML = address
-            }, 100);
-        }
-    });
+  document.getElementById('get_started').onclick = function() {
+    document.getElementById('title').style.animation = "exiting 1.5s forwards";
+    document.getElementById('explanation').style.animationDelay = "2.5s";
+    document.getElementById('explanation').style.animation = "exiting 1.5s forwards";
+    document.getElementById('get_started').style.animationDelay = "3.5s";
+    document.getElementById('get_started').style.animation = "exiting 1.5s forwards";
+    document.getElementById('surveyTitle').innerHTML = surveyTitles[0];
+    document.getElementById('surveyTitle').style.animationDelay = "3.5s";
+    document.getElementById('surveyTitle').style.animation = "startSurveyTitle 2.5s forwards";
+    document.getElementById('loading').style.animationDelay = "3.5s";
+    document.getElementById('loading').style.animation = "show 1s forwards";
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        coords.lat = position.coords.latitude;
+        coords.long = position.coords.longitude;
+        getReverseGeocodingData(position.coords.latitude, position.coords.longitude);
+        document.getElementById('loading').style.opacity = "0";
+      });
+    }
+  }
+  function getPredictHQ(token) {
+   // let get = new XMLHttpRequest()
+   //  get.open('POST','https://api.predicthq.com/v1/account/?within=10km@-36.844480,174.768368')
+   //  get.setRequestHeader('Authorization', 'Bearer ' + api_key)
+    //7uoUm2GEVjCDqsLLEBA2GEBoeRnfSiWZTUEowVHC
+
+  var phq = new Client({access_token: "7uoUm2GEVjCDqsLLEBA2GEBoeRnfSiWZTUEowVHC"})
+  var find = phq.events.search({radius:'10m',latitude:cords.lat,longtiude:cords.long})
+  console.log(find)
+  }
+  function getReverseGeocodingData(lat, lng) {
+      var latlng = new google.maps.LatLng(lat, lng);
+      // This is making the Geocode request
+      var geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+          if (status !== google.maps.GeocoderStatus.OK) {
+              alert(status);
+          }
+          // This is checking to see if the Geoeode Status is OK before proceeding
+          if (status == google.maps.GeocoderStatus.OK) {
+              setTimeout(function() {
+                address = results[0].formatted_address;
+                document.getElementById('location').classList.add("locationClass");
+                document.getElementById('location').innerHTML = address
+              }, 100);
+          }
+      });
+  }
 }
