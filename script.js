49ec80bc55019e4d1906866787539b28d0951190
ozanmirza1 <ozan.mirza@icloud.com>
2018-06-16 10:49:53 -0400

    Ozan Is Awesome

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.getElementById('get_started').onclick = function() {
-  document.getElementById('title').style.animation = "exiting 2.5s forwards";
+  document.getElementById('title').style.animation = "exiting 1.5s forwards";
   document.getElementById('explanation').style.animationDelay = "2.5s";
-  document.getElementById('explanation').style.animation = "exiting 2.5s forwards";
+  document.getElementById('explanation').style.animation = "exiting 1.5s forwards";
+  document.getElementById('get_started').style.animationDelay = "3.5s";
+  document.getElementById('get_started').style.animation = "exiting 1.5s forwards";
 }
