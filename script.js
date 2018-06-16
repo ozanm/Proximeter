4426fbc1718ab6816dcb672a1ae7f360f60e89fc
ozanmirza1 <ozan.mirza@icloud.com>
2018-06-16 10:32:46 -0400

    Ozan Is Awesome

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById('get_started').onclick = function() {
   document.getElementById('title').classList.add("exit");
+  console.log("added class");
   setTimeout(750, function() {
     document.getElementById('explanation').classList.add("exit");
   });
